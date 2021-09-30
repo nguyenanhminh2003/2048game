a76fc4c108ec5a16f89be5f407daf186bb9cb2b7
nguyenanhminh2003 <nguyenanhminh5dict@gmail.com>
2021-09-30 15:54:49 +0700

    2048Finished

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 class game {
-  score = 0;
+    score = 0;
+    arr = [0, 0, 0, 0];
+
     constructor() {
         this.canvas = null;
         this.context = null;
@@ -15,6 +17,8 @@ class game {
         this.addNum();
         this.draw();
         this.handle();
+        this.checkForWin();
+
 
     }
 
@@ -27,25 +31,71 @@ class game {
         document.body.appendChild(this.canvas);
     }
 
-    addNum(){
-        let arr = [];
-        for (let i = 0; i < 4; i++){
-            for (let j = 0; j < 4; j++){
-                if(this.grid[i][j] == 0){
-                    arr.push({x:i, y:j});
+
+
+    checkMovable(oldGrid, newGrid) {
+        let count = 0;
+        let count0 = 0;
+        for (let i = 0; i < oldGrid.length; i++) {
+            for (let j = 0; j < newGrid.length; j++) {
+                if (oldGrid[i][j] == newGrid[i][j]) {
+                    count++;
+                }
+                if (newGrid[i][j] == 0) {
+                    count0++
                 }
             }
         }
-        if (arr.length > 0){
+        return count == 16 && count0 == 0 ? true : false;
+    }
+
+    checkLoss(arr) {
+        let count = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+            }
+        }
+        if (count == 4) {
+            alert('Loss')
+            location.reload()
+
+
+        }
+
+    }
+
+    checkForWin() {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (this.grid[i][j] == 2048) {
+                    alert('u win')
+                    location.reload()
+                }
+            }
+        }
+    }
+
+    addNum() {
+        let arr = [];
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (this.grid[i][j] == 0) {
+                    arr.push({x: i, y: j});
+                }
+            }
+        }
+        if (arr.length > 0) {
             let randomXY = arr[Math.random() * arr.length >> 0];
-            let num = Math.floor(Math.random()*4);
-            if (num < 3){
+            let num = Math.floor(Math.random() * 4);
+            if (num < 3) {
                 this.grid[randomXY.x][randomXY.y] = 2;
-            }else{
+            } else {
                 this.grid[randomXY.x][randomXY.y] = 4;
             }
 
         }
+        this.score++;
     }
 
     draw() {
@@ -62,37 +112,37 @@ class game {
         }
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
-                if(this.grid[i][j] == 2){
-                    this.context.fillStyle ='#FFFFFF';
-                    this.context.fillRect(j * 100+1, i *100+1, 98, 98);
-                } else if(this.grid[i][j] == 4){
-                    this.context.fillStyle ='#FFFFCC';
-                    this.context.fillRect(j * 100+1, i *100+1, 98, 98);
-                }else if(this.grid[i][j] == 8) {
+                if (this.grid[i][j] == 2) {
+                    this.context.fillStyle = '#FFFFFF';
+                    this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
+                } else if (this.grid[i][j] == 4) {
+                    this.context.fillStyle = '#FFFFCC';
+                    this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
+                } else if (this.grid[i][j] == 8) {
                     this.context.fillStyle = '#FFCC33';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 16) {
+                } else if (this.grid[i][j] == 16) {
                     this.context.fillStyle = '#FF9966';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 32) {
+                } else if (this.grid[i][j] == 32) {
                     this.context.fillStyle = '#FF6633';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 64) {
+                } else if (this.grid[i][j] == 64) {
                     this.context.fillStyle = '#CC3300';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 128) {
+                } else if (this.grid[i][j] == 128) {
                     this.context.fillStyle = '#FFFF33';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 256) {
+                } else if (this.grid[i][j] == 256) {
                     this.context.fillStyle = '#3366CC';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 512) {
+                } else if (this.grid[i][j] == 512) {
                     this.context.fillStyle = '#FF33FF';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 1024) {
+                } else if (this.grid[i][j] == 1024) {
                     this.context.fillStyle = '#003366';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
-                }else if(this.grid[i][j] == 2048) {
+                } else if (this.grid[i][j] == 2048) {
                     this.context.fillStyle = '#00DD00';
                     this.context.fillRect(j * 100 + 1, i * 100 + 1, 98, 98);
                 }
@@ -137,9 +187,9 @@ class game {
         return arr;
     }
 
-    hasChangeRC(arr1, arr2){
-        for(let i = 0; i < 4; i++){
-            if(arr1[i] != arr2[i]){
+    hasChangeRC(arr1, arr2) {
+        for (let i = 0; i < 4; i++) {
+            if (arr1[i] != arr2[i]) {
                 this.hasChange = true;
             }
         }
@@ -147,96 +197,120 @@ class game {
 
     handle() {
         document.addEventListener('keydown', (e) => {
-            this.score++
+            this.checkForWin()
             document.getElementById('diemSo').innerHTML = this.score
             this.hasChange = false;
+            let oldGrid = [...this.grid]
             if (e.which == 37) {
+                // this.checkForLoss();
+                if (this.checkMovable(oldGrid, this.grid)) {
+                    this.arr[0] = 1;
+                }
+                this.checkLoss(this.arr);
                 for (let i = 0; i < 4; i++) {
                     let arr = this.grid[i];
                     this.grid[i] = this.slideLeftOrUp(this.grid[i]);
-                    for(let j = 0; j <3; j++){
-                        if(this.grid[i][j] == this.grid[i][j+1]){
-                            this.grid[i][j] += this.grid[i][j+1];
-                            this.grid[i][j+1] = 0;
+                    for (let j = 0; j < 3; j++) {
+                        if (this.grid[i][j] == this.grid[i][j + 1]) {
+                            this.grid[i][j] += this.grid[i][j + 1];
+                            this.grid[i][j + 1] = 0;
                         }
                     }
                     this.grid[i] = this.slideLeftOrUp(this.grid[i]);
                     this.hasChangeRC(arr, this.grid[i]);
                 }
-                if(this.hasChange) {
+                if (this.hasChange) {
                     this.addNum();
                 }
                 this.draw();
             } else if (e.which == 38) {
-                for(let i = 0; i < 4; i++){
-                    let arr =[];
-                    for(let j = 0; j < 4; j++){
+                if (this.checkMovable(oldGrid, this.grid)) {
+                    this.arr[1] = 1;
+                }
+                this.checkLoss(this.arr);
+                for (let i = 0; i < 4; i++) {
+                    let arr = [];
+                    for (let j = 0; j < 4; j++) {
                         arr.push(this.grid[j][i]);
                     }
                     let arr1 = arr;
                     arr = this.slideLeftOrUp(arr);
-                    for(let m = 0;m <3; m++){
-                        if(arr[m] == arr[m+1]){
-                            arr[m] += arr[m+1];
-                            arr[m+1] = 0;
+                    for (let m = 0; m < 3; m++) {
+                        if (arr[m] == arr[m + 1]) {
+                            arr[m] += arr[m + 1];
+                            arr[m + 1] = 0;
                         }
                     }
                     arr = this.slideLeftOrUp(arr);
-                    for(let m = 0; m < 4; m++) {
+                    for (let m = 0; m < 4; m++) {
                         this.grid[m][i] = arr[m];
                     }
                     this.hasChangeRC(arr, arr1);
                 }
-                if(this.hasChange) {
+                if (this.hasChange) {
                     this.addNum();
                 }
                 this.draw()
             } else if (e.which == 39) {
+                if (this.checkMovable(oldGrid, this.grid)) {
+                    this.arr[2] = 1;
+                }
+                this.checkLoss(this.arr);
                 for (let i = 0; i < 4; i++) {
                     let arr = this.grid[i];
                     this.grid[i] = this.slideRightOrDown(this.grid[i]);
-                    for(let j = 3; j > 0; j--){
-                        if(this.grid[i][j] == this.grid[i][j-1]){
-                            this.grid[i][j] += this.grid[i][j-1];
-                            this.grid[i][j-1] = 0;
+                    for (let j = 3; j > 0; j--) {
+                        if (this.grid[i][j] == this.grid[i][j - 1]) {
+                            this.grid[i][j] += this.grid[i][j - 1];
+                            this.grid[i][j - 1] = 0;
                         }
                     }
                     this.grid[i] = this.slideRightOrDown(this.grid[i]);
                     this.hasChangeRC(arr, this.grid[i]);
                 }
-                if(this.hasChange) {
+                if (this.hasChange) {
                     this.addNum();
                 }
 
                 this.draw();
             } else if (e.which == 40) {
-                for(let i = 0; i < 4; i++){
-                    let arr =[];
-                    for(let j = 0; j < 4; j++){
+                if (this.checkMovable(oldGrid, this.grid)) {
+                    this.arr[3] = 1;
+                }
+                this.checkLoss(this.arr);
+                for (let i = 0; i < 4; i++) {
+                    let arr = [];
+                    for (let j = 0; j < 4; j++) {
                         arr.push(this.grid[j][i]);
                     }
                     let arr1 = arr;
                     arr = this.slideRightOrDown(arr);
-                    for(let m = 3;m >0; m--){
-                        if(arr[m] == arr[m-1]){
-                            arr[m] += arr[m-1];
-                            arr[m-1] = 0;
+                    for (let m = 3; m > 0; m--) {
+                        if (arr[m] == arr[m - 1]) {
+                            arr[m] += arr[m - 1];
+                            arr[m - 1] = 0;
                         }
                     }
                     arr = this.slideRightOrDown(arr);
-                    for(let m = 0; m < 4; m++) {
+                    for (let m = 0; m < 4; m++) {
                         this.grid[m][i] = arr[m];
                     }
                     this.hasChangeRC(arr, arr1);
                 }
-                if(this.hasChange) {
+                if (this.hasChange) {
                     this.addNum();
                 }
                 this.draw()
             }
+
+
         })
+
+
     }
 
 }
 
+
 var g = new game()
+
